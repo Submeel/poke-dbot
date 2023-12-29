d3c5d4c91d4142eaf62863b7eb4098d841c737c9
Submeel <155126671+Submeel@users.noreply.github.com>
2023-12-30 07:38:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = process.env.BOT_TOKEN;
 const SpreadsheetDataHandler = require('./sheet.js');
 const _ = require('lodash');
 
