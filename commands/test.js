e4537623f942894e98d8a8dda9942e40e1f71b2d
Submeel <155126671+Submeel@users.noreply.github.com>
2023-12-31 21:21:32 +0900

    되려나...
    도전이나해봄...

---
+++ commands/test.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('discord.js');
+const SpreadsheetDataHandler = require('../sheet.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('판매')
+    .setDescription('아이템을 판매합니다')
+    .addStringOption(option =>
+      option
+        .setName('아이템')
+        .setDescription('구매할 아이템을 입력해 주세요.')
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName('수량')
+        .setDescription('1이상 99미만의 정수로 판매할 수량을 입력해 주세요. 공란일 경우 1개로 취급합니다.')
+    )
+  ,
+
+  async execute(interaction) {
+    const userId = interaction.user.id;
+    const item = interaction.options.getString('아이템')
+    const amount = interaction.options.getInteger('수량')
+    let result = doTest(item, amount, userId)
+    const confirm = new ButtonBuilder()
+      .setCustomId('confirm')
+      .setLabel('✅')
+      .setStyle(ButtonStyle.Success);
+    const cancel = new ButtonBuilder()
+      .setCustomId('cancel')
+      .setLabel('❌')
+      .setStyle(ButtonStyle.Danger); 
+    const row = new ActionRowBuilder()
+      .addComponents(cancel, confirm);
+    // if (result.hasOwnProperty('updateData')) {
+    //   const dataHandler = SpreadsheetDataHandler.getInstance();
+    //   await dataHandler.updateCells(result.updateData)
+    //   dataHandler.sheetRecords = result.sheetRecords
+    // }
+    await interaction.reply({
+      content: result.content, 
+      components: [row]
+    });
+  },
+};
