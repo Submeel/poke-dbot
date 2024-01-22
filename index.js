524ce9244d97d1975e56afd00ce3cb9439fee045
Submeel <155126671+Submeel@users.noreply.github.com>
2024-01-22 21:50:20 +0900

    임시로 되돌리기

--- index.js
+++ index.js
@@ -5,16 +5,9 @@ const config = require('./config.json');
 const token = process.env.BOT_TOKEN || config.token;
 const SpreadsheetDataHandler = require('./sheet.js');
 const _ = require('lodash');
-const { useSkill } = require('./normalAction/useSkill');
 
 
-// 해당 권한들이 있어야 일반 메세지를 인식합니다
-const client = new Client({ intents: [GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMessageTyping,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildModeration,
-    GatewayIntentBits.MessageContent] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 
 client.commands = new Collection();
@@ -64,7 +57,7 @@ client.on(Events.ClientReady, () => {
 });
 
 
-// Slash 커맨드일 경우 반응
+
 client.on(Events.InteractionCreate, async interaction => {
     console.log('Events.InteractionCreate');
 
@@ -88,69 +81,10 @@ client.on(Events.InteractionCreate, async interaction => {
 });
 
 
-
-// 일반 메세지에 [] 가 있을 경우 반응
-client.on(Events.MessageCreate, async message => {
-    // 봇 자신의 메시지는 무시
-    if (message.author == client.user) return;
-
-    // 메시지 내의 대괄호 [] 순서 확인
-    const keywords = getBracketContents(message.content);
-
-    if (keywords.length > 0) {
-        if (keywords[0] === '') return
-        
-        let result = await checkKeywords(keywords, message.author.id)
-        if (result!= '' && result.content !== null && result.content !== '' && result.content !== undefined){
-            try{
-                message.reply(result.content)
-                if (result.hasOwnProperty('updateData')) {
-                    // 업데이트
-                    const dataHandler = SpreadsheetDataHandler.getInstance();
-                    await dataHandler.updateCells(result.updateData)
-                    dataHandler.sheetRecords = result.sheetRecords
-                }
-            } catch (e){
-                const content = `예기치 못한 오류가 발생했습니다. 총괄에게 문의하세요. name:${e.name}, message:${e.message}, stack:${e.stack}`
-                message.reply(content)
-            }
-        }
-    } 
-});
-
-
-// 대괄호 안의 내용을 가져오는 함수
-function getBracketContents(text) {
-	const bracketContents = [];
-	const regex = /\[([^\]]*)\]/g;
-
-	let match;
-	while ((match = regex.exec(text)) !== null) {
-		bracketContents.push(match[1].split('/').map(item => item.trim()));
-	}
-
-	return bracketContents.flat();
-}
-
-
-async function checkKeywords(keywords, userId){
-	
-	console.log('###### checkKeywords start:::', keywords)
-	let result = ''
-	if (keywords[0] === '기술사용'){
-		result = await useSkill(keywords, userId)
-	}
-
-
-	return result
-
-}
-
-
 (async () => {
     try {
         await dataHandler.getAllSheetDataAsDictionaries();
-        // console.log('Sheet data load 완료:', dataHandler.sheetRecords);
+        console.log('Sheet data load 완료:', dataHandler.sheetRecords);
         client.login(token);
     } catch (e) {
         console.log('e:', e);
