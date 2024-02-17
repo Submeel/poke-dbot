fb84125cd0016037c764a82ac74149dd113af0f5
Submeel <155126671+Submeel@users.noreply.github.com>
2024-02-18 00:49:36 +0900

    이거던가...

--- action/doPokemonExp.js
+++ action/doPokemonExp.js
@@ -126,7 +126,7 @@ function doPokemonExp(monName, amount, userId) {
     let levelUpDesc = '';
     if (monLevel < tobeMonLv) {
       levelUpDesc = `\n ▶${monName}의 레벨이 ${tobeMonLv} (으)로 올랐다!\n `
-      if (allPkmObjs[monIdx]['파티'] === 'TRUE'){
+      if (allPkmObjs[monIdx]['파티'] === true){
         levelUpDesc =+`▶HP가 올랐다! [ ${chaMaxHp} → ${chaMaxHp + (tobeMonLv - monLevel) * 6} ]\n ▶HP가 모두 회복되었다!`; //임베드에 레벨업 메시지 보내기
         chaMaxHp = chaMaxHp + (tobeMonLv - monLevel) * 6
         chaNowHp = chaMaxHp} //렙업은 최고의 힐이다
